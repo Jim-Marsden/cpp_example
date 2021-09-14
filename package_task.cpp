eff3d29937e88308f68d06a0bc2a89f9872e54ab
Jim <JamesPMarsden@hotmail.com>
2021-09-14 13:42:20 -0700

    added pack_taask.cpp

---
+++ package_task.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Jim on 9/14/21.
+//
+#include <future>
+
+auto package_task_main(){
+
+    return 0;
+
+}
+
+static auto v = package_task_main();
+// to run before main, or you can make this self contained,
+// and remove this, and rename 'package_task_main' to just main.
